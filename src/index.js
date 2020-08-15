0b181d0ed4a7e15ecca394bc1f7fdb14dbffcb92
David Gethers <dgethers.jr@gmail.com>
2020-08-15 14:07:21 -0400

    Robo friends are read from external file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import CardList from './CardList';
+import Card from "./Card";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { robots } from "./robots";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
