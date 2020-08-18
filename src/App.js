96f748d84e5364a3918735a1aded36f9edf8a427
David Gethers <dgethers.jr@gmail.com>
2020-08-18 19:31:48 -0400

    Create SearchBox component. Change App to class instead of arrow function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Robo Friends</h1>
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    };
+  }
+
+  onSearchChange(event) {
+      console.log(event);
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>Robo Friends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
