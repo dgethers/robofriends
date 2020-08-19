aa6cc28cff8ad97596d2139115b96ebd158bc072
David Gethers <dgethers.jr@gmail.com>
2020-08-19 11:35:23 -0400

    Add Redux Prodiver for search robots

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll"
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
@@ -28,18 +28,14 @@ class App extends Component {
   };
 
   render() {
-    const {robots, searchfield} = this.state;
+    const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
     if (!robots) {
-      
       return <h1>Loading...</h1>;
     } else {
-      
       return (
         <div className="tc">
           <h1 className="f2">Robo Friends</h1>
