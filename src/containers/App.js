b7bc211058ffbc03045b8d0bc496198ee9ba0613
David Gethers <dgethers.jr@gmail.com>
2020-08-21 11:37:19 -0400

    Refactoring according to course material

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,14 +12,13 @@ const mapStateToProps = (state) => {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => requestRobots(dispatch),
+    onRequestRobots: () => dispatch(requestRobots()),
   };
 };
 
@@ -34,21 +33,21 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    if (isPending) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f2">Robo Friends</h1>
-          <SearchBox searchChange={onSearchChange} />
-          <Scroll>
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          {isPending ? (
+            <h1>Loading...</h1>
+          ) : (
             <ErrorBoundry>
               <CardList robots={filteredRobots} />
             </ErrorBoundry>
-          </Scroll>
-        </div>
-      );
-    }
+          )}
+        </Scroll>
+      </div>
+    );
   }
 }
 
